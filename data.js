9eb50a4daa1a7dce1b0d6b1aca18bbf697ef9537
Zigax201 <zigax201@gmail.com>
2020-10-28 15:35:03 +0700

    Upload Object

---
+++ data.js
@@ -0,0 +1,31 @@
+var text = [{
+  "PEMIN":[{
+    "TOKOA": [{
+        "NAMA": "OLSHOPBABY",
+        "OWNER": "JOKO",
+        "PRODUK": [{
+                "NAMA_PRODUK": "DIAPERS",
+                "STOK": "",
+                "HARGA": ""
+            },{
+                "NAMA_PRODUK": "SUSU",
+                "STOK": "",
+                "HARGA": ""
+            },{
+                "NAMA_PRODUK": "BAJUBAYI",
+                "STOK": "",
+                "HARGA": ""
+            }]
+        }]
+    }]
+  }]
+
+$.ajax({
+  url:"index.php",
+  method: "POST",
+  data: {data:JSON.stringify(text)},
+  // contentType:'application/json',
+  success: function(res){
+    console.log(res)
+  }
+})
